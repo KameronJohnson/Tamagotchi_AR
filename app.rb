c16b47136de170cd191015786ec88352dde3e786
Kameron Ross Johnson <KameronJohnson@gmail.com>
2015-02-03 16:08:28 -0800

    add initial sinatra functionality & index.erb file

--- app.rb
+++ app.rb
@@ -2,3 +2,19 @@ require 'bundler/setup'
 Bundler.require :default
 
 Dir[File.dirname(__FILE__)+'/lib/*.rb'].each { |file| require file}
+
+get '/' do
+  @tamagotchis = Tamagotchi.all
+  erb(:index)
+end
+
+post '/tamagotchi_add' do
+  name = params['name']
+  @tamagotchi = Tamagotchi.create({:name => name})
+  redirect '/'
+end
+
+# get 'tamagotchi/:id' do
+#   @tamagotchi = Tamagotchi.find(params['id'])
+#   # erb :tamagotchi
+# end
